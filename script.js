65be555f163c5259150dc6463a802578f2d18fac
rahulkuma9503-coder <rahulkuma9503@gmail.com>
2025-12-07 03:01:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Initialize Telegram Mini App
 const tg = window.Telegram.WebApp;
 tg.expand();
 
-document.getElementById("joinBtn").addEventListener("click", () => {
-    // 🚨 Yahan apna group/bot link daalo
+document.getElementById("joinBtn").onclick = () => {
+    // Yahan apna link lagao
     window.location.href = "https://t.me/YOUR_GROUP_LINK";
-});
+};
